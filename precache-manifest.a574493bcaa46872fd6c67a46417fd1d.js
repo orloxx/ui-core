17e0d00ca6b61300afd3f5cc4466f281d6f0f805
Iker Garitaonandia <iker.garitaonandia@netcentric.biz>
2019-04-27 15:43:19 +0200

    Updates

---
+++ precache-manifest.a574493bcaa46872fd6c67a46417fd1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42f18be53fd9dea5bf10bcfef59129a",
+    "url": "/ui-core/index.html"
+  },
+  {
+    "revision": "f5902ae85261cb92e6f2",
+    "url": "/ui-core/static/css/main.9516b4b6.chunk.css"
+  },
+  {
+    "revision": "53499a926822073848d6",
+    "url": "/ui-core/static/js/2.48d84852.chunk.js"
+  },
+  {
+    "revision": "f5902ae85261cb92e6f2",
+    "url": "/ui-core/static/js/main.b75aadf4.chunk.js"
+  },
+  {
+    "revision": "095f548cf1e45b78f9bc",
+    "url": "/ui-core/static/js/runtime~main.af5faabe.js"
+  }
+]);
