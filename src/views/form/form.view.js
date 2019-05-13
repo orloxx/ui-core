f2fb5b36de48836ee1ce7ccf5cafedf41851fd79
Iker Garitaonandia <iker.garitaonandia@netcentric.biz>
2019-05-13 18:24:12 +0200

    Validates on submit

--- src/views/form/form.view.js
+++ src/views/form/form.view.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Controls the form element and validates all children when trying to submit
+ *
+ * @extends {react~Component}
+ *
+ * @example
+ * <Form action='http://someaction.example.com/'>
+ *   <ImageField id='avatar' name='avatar' label='Avatar' />
+ *   <Field id='username' name='username'
+ *     label='Username' placeholder='Enter your username'
+ *     suggestion='Only accepts letters and numbers'
+ *     patternError='Input format is wrong, only accepts letters and numbers'
+ *     required pattern='^[a-zA-Z0-9]+$' />
+ *   <Field id='name' name='name'
+ *     label='Name' placeholder='Enter your full name' />
+ *   <EmailField id='email' name='email'
+ *     label='Email' placeholder='Enter your email address' />
+ *   <DropdownField id='sex' name='sex' label='Sex' placeholder='Select one' required>
+ *     <option value='M'>Male</option>
+ *     <option value='F'>Female</option>
+ *   </DropdownField>
+ *   <TextareaField id='comments' name='comments' label='Comments'
+ *     suggestion='This field has a 100 character limit'
+ *     maxChars={100} maxCharsError='Character limit reached' />
+ * </Form>
+ */
 class Form extends Component {
   /**
    * @type {Object}
@@ -24,8 +50,36 @@ class Form extends Component {
   /**
    * @ignore
    */
-  renderChildren() {
-    return this.props.children.map(el => el);
+  constructor(props) {
+    super(props);
+
+    /**
+     * @ignore
+     */
+    this.instances = {};
+
+    /**
+     * @ignore
+     */
+    this.children = this.props.children.map(el => React.cloneElement(el, {
+      key: el.props.id,
+      ref: inst => { this.instances[el.props.id] = inst; },
+    }));
+  }
+
+  /**
+   * @ignore
+   */
+  onSubmit(e) {
+    e.preventDefault();
+    const invalidFields = Object.keys(this.instances).filter((key) => {
+      const component = this.instances[key];
+      component.onBlur();
+      return !component.isValid;
+    });
+    if (!invalidFields.length) {
+      console.log('SUBMIT!');
+    }
   }
 
   /**
@@ -34,8 +88,11 @@ class Form extends Component {
   render() {
     const { action, method } = this.props;
     return (
-      <form action={action} method={method} noValidate>
-        {this.renderChildren()}
+      <form
+        action={action} method={method}
+        onSubmit={e => this.onSubmit(e)}
+        noValidate>
+        {this.children}
         <div className='buttonGroup'>
           <button className='button button--secondary' type='button'>Cancel</button>
           <button className='button' type='submit'>Save</button>
