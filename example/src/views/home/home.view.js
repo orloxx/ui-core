440e65993df063bfb352ca9260f948fd4972db0b
Iker Garitaonandia <iker.garitaonandia@netcentric.biz>
2020-05-23 16:46:20 +0200

    Updates btc and removes it from readme

--- example/src/views/home/home.view.js
+++ example/src/views/home/home.view.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import bitcoinImg from './bitcoin.jpg';
 
 export class Home extends Component {
+  static get BTC() {
+    return '1MuQotL54dgTG1ounhkZtMB3DjPUP1yQ6C';
+  }
+
   render() {
     return (
       <section className='home container'>
@@ -10,9 +14,9 @@ export class Home extends Component {
         <ul>
           <li><a href='https://orloxx.github.io/ui-core/docs/'>JS documentation</a></li>
         </ul>
-        <p>I can always use a beer <span role='img' aria-label='Oh beer'>&#x1F37A;</span> or two :)</p>
-        <p>Bitcoin: 1sSBBuBgnapZ1Zxmf9PYoBMxv8kN6Wggb</p>
+        <h4>Donate (<span title='Bitcoin' aria-label='Bitcoin'>&#x20BF;itcoin</span>)</h4>
         <p><img src={bitcoinImg} alt='Donate Bitcoin' /></p>
+        <p>{Home.BTC}</p>
         <p><a href="https://github.com/orloxx/ui-core">Return to Github Repository</a></p>
       </section>
     );
